c09e4d077a175f6a5791d69013fa65941c9a64f7
harrybell1995 <hcbnffc@gmail.com>
2024-12-06 18:49:48 +0000

    all formations and id's added

--- src/components/FormationDisplay.tsx
+++ src/components/FormationDisplay.tsx
@@ -3,60 +3,34 @@ import { Tooltip } from './Tooltip';  // Assuming Tooltip is a separate componen
 
 // Mapping formation IDs to formation strings
 const formationMapping: Record<string, string> = {
-  '36': '4-2-1-3',  // ID '36' corresponds to '4-2-1-3'
-  '2': '4-1-4-1',    // ID '2' corresponds to '4-1-4-1'
-  '16': '4-4-2',     // ID '16' corresponds to '4-4-2'
-  '24': '3-4-2-1',   // ID '24' corresponds to '3-4-2-1'
-  '23': '3-4-1-2',   // ID '23' corresponds to '3-4-1-2'
-  '25': '3-4-3',     // ID '25' corresponds to '3-4-3'
-  '27': '3-5-2',     // ID '27' corresponds to '3-5-2'
-
-  // Add more mappings for other formations when IDs are known
-  // 'ID_here': '3-1-4-2',
-  // 'ID_here': '3-4-3 Diamond',
-  // 'ID_here': '3-4-3 Flat',
-  // 'ID_here': '3-5-1-1',
-  // 'ID_here': '4-1-2-1-2',
-  // 'ID_here': '4-1-2-1-2 (2)',
-  // 'ID_here': '4-1-2-1-2 Narrow',
-  // 'ID_here': '4-1-2-1-2 Wide',
-  // 'ID_here': '4-1-3-2',
-  // 'ID_here': '4-2-2-2',
-  // 'ID_here': '4-2-3-1',
-  // 'ID_here': '4-2-3-1 (2)',
-  // 'ID_here': '4-2-3-1 Narrow',
-  // 'ID_here': '4-2-3-1 Wide',
-  // 'ID_here': '4-2-4',
-  // 'ID_here': '4-3-1-2',
-  // 'ID_here': '4-3-2-1',
-  // 'ID_here': '4-3-3',
-  // 'ID_here': '4-3-3 (2)',
-  // 'ID_here': '4-3-3 (3)',
-  // 'ID_here': '4-3-3 (4)',
-  // 'ID_here': '4-3-3 (5)',
-  // 'ID_here': '4-3-3 Attack',
-  // 'ID_here': '4-3-3 Defend',
-  // 'ID_here': '4-3-3 False 9',
-  // 'ID_here': '4-3-3 Flat',
-  // 'ID_here': '4-3-3 Holding',
-  // 'ID_here': '4-4-1-1 (2)',
-  // 'ID_here': '4-4-1-1 Attack',
-  // 'ID_here': '4-4-1-1 Midfield',
-  // 'ID_here': '4-4-2 (2)',
-  // 'ID_here': '4-4-2 Flat',
-  // 'ID_here': '4-4-2 Holding',
-  // 'ID_here': '4-5-1',
-  // 'ID_here': '4-5-1 (2)',
-  // 'ID_here': '4-5-1 Attack',
-  // 'ID_here': '4-5-1 Flat',
-  // 'ID_here': '5-1-2-2',
-  // 'ID_here': '5-2-1-2',
-  // 'ID_here': '5-2-2-1',
-  // 'ID_here': '5-2-3',
-  // 'ID_here': '5-3-2',
-  // 'ID_here': '5-4-1',
-  // 'ID_here': '5-4-1 Diamond',
-  // 'ID_here': '5-4-1 Flat',
+  '36': '4-2-1-3',   // ID '36' corresponds to '4-2-1-3'
+  '2': '4-1-4-1',     // ID '2' corresponds to '4-1-4-1'
+  '16': '4-4-2',      // ID '16' corresponds to '4-4-2'
+  '24': '3-4-2-1',    // ID '24' corresponds to '3-4-2-1'
+  '23': '3-4-1-2',    // ID '23' corresponds to '3-4-1-2'
+  '25': '3-4-3',      // ID '25' corresponds to '3-4-3'
+  '27': '3-5-2',      // ID '27' corresponds to '3-5-2'
+  '33': '5-4-1',      // ID '33' corresponds to '5-4-1'
+  '31': '5-3-2',      // ID '31' corresponds to '5-3-2'
+  '30': '5-2-3',      // ID '30' corresponds to '5-2-3'
+  '29': '5-2-1-2',    // ID '29' corresponds to '5-2-1-2'
+  '20': '4-5-1 (2)',  // ID '20' corresponds to '4-5-1 (2)'
+  '21': '4-5-1',      // ID '21' corresponds to '4-5-1'
+  '17': '4-4-2 (2)',  // ID '17' corresponds to '4-4-2 (2)'
+  '18': '4-4-1-1',    // ID '18' corresponds to '4-4-1-1'
+  '11': '4-3-3 (4)',  // ID '11' corresponds to '4-3-3 (4)'
+  '10': '4-3-3 (3)',  // ID '10' corresponds to '4-3-3 (3)'
+  '9': '4-3-3 (2)',   // ID '9' corresponds to '4-3-3 (2)'
+  '8': '4-3-3',       // ID '8' corresponds to '4-3-3'
+  '7': '4-3-2-1',     // ID '7' corresponds to '4-3-2-1'
+  '6': '4-3-1-2',     // ID '6' corresponds to '4-3-1-2'
+  '5': '4-2-4',       // ID '5' corresponds to '4-2-4'
+  '4': '4-2-3-1 (2)', // ID '4' corresponds to '4-2-3-1 (2)'
+  '3': '4-2-3-1',     // ID '3' corresponds to '4-2-3-1'
+  '13': '4-2-2-2',    // ID '13' corresponds to '4-2-2-2'
+  '15': '4-1-2-1-2 (2)', // ID '15' corresponds to '4-1-2-1-2 (2)'
+  '14': '4-1-2-1-2',  // ID '14' corresponds to '4-1-2-1-2'
+  '1': '4-1-3-2'      // ID '1' corresponds to '4-1-3-2'
 };
 
 interface FormationDisplayProps {
